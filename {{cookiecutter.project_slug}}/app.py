fc6285a1b832c78089e3065bfd94a4dd24535d62
raysaavedra-work <raysaavedra.work@gmail.com>
2019-10-02 00:42:42 +0800

    add fix

--- {{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_slug}}/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__, template_folder='client')
 
 @app.route('/')
 def order():
-    api_key = os.getenv('STRIPE_SECRET_KEY')
+    api_key = os.getenv('STRIPE_PUBLIC_KEY')
     return render_template('order.html', api_key=api_key)
 
 @app.route('/order_success')
